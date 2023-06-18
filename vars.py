3f4d17d5ec97f54f29095e425ea5c393c12cf9f7
Star Bots Tamil <126967523+Star-Bots-Tamil@users.noreply.github.com>
2023-06-19 04:41:23 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -11,11 +11,12 @@ def is_enabled(value, default):
         return default
 
 SESSION = environ.get("SESSION", "forward bot")
-API_ID = int(environ["API_ID"])
-API_HASH = environ["API_HASH"]
-BOT_TOKEN = environ["BOT_TOKEN"]
+API_ID = int(environ.get('API_ID', '11973721'))
+API_HASH = environ.get('API_HASH', '5264bf4663e9159565603522f58d3c18')
+BOT_TOKEN = environ.get('BOT_TOKEN', '5965670031:AAE_nwYbYT9rb3y1H3xjiyVEb8Di2UpSM3c')
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", 0))
 PORT = int(environ.get("PORT", "8080"))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1391556668').split()]
+FILE_CAPTION = environ.get('FILE_CAPTION', '<b>{file_name}</b>')
 TARGET_DB = int(environ.get("TARGET_DB", 0))
-UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/Joelkb/File-Forward-Bot")
+UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/Star-Bots-Tamil/File-Forward-Star-Bots/tree/web")
